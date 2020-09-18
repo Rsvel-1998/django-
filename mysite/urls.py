0195e408d4ba82ea0ae66f1787c0d00e672bf967
Rsvel-1998 <70759631+Rsvel-1998@users.noreply.github.com>
2020-09-18 07:41:30 +0530

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
-#app_name = 'myapp',
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('myapp/', include('myapp.urls')),
