fa44b3953e8214d75b92430e6c59f148fd6156f8
Rsvel-1998 <70759631+Rsvel-1998@users.noreply.github.com>
2020-09-18 07:51:41 +0530

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -134,11 +134,3 @@ def vote(request, question_id):
          #with POST data. This prevents data from being posted twice if a
          #user hits the Back button.
         return HttpResponseRedirect(reverse('myapp:results', args=(question.id,)))
-  
-    
-    
-    
-    
-    
-    
-    	
